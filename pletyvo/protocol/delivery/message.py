c6fd9c8f89be50b2533fc896ab5ffacf72747f86
Mykola Peredrii <mykolaperedrii@proton.me>
2025-04-07 20:58:05 +0200

    feat: implement `delivery.MessageInput`

--- pletyvo/protocol/delivery/message.py
+++ pletyvo/protocol/delivery/message.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 
 __all__: typing.Sequence[str] = (
     "Message",
+    "MessageInput",
 )
 
 import typing
@@ -14,10 +15,6 @@ import attrs
 
 from pletyvo.protocol import dapp
 
-if typing.TYPE_CHECKING:
-    from pletyvo.types import (
-        UUIDLike,
-    )
 
 message_content_validator = (
     attrs.validators.min_len(1),
@@ -27,9 +24,9 @@ message_content_validator = (
 
 @attrs.define(hash=True)
 class Message:
-    body: dapp.EventBody = attrs.field(converter=lambda d: dapp.EventBody.from_str(d))
+    body: dapp.EventBody = attrs.field(converter=lambda s: s if isinstance(s, dapp.EventBody) else dapp.EventBody.from_str(s))
 
-    auth: dapp.AuthHeader = attrs.field(converter=lambda d: dapp.AuthHeader.from_dict(d))
+    auth: dapp.AuthHeader = attrs.field(converter=lambda d: d if isinstance(d, dapp.AuthHeader) else dapp.AuthHeader.from_dict(d))
 
     @classmethod
     def from_dict(cls, d: dict[str, typing.Any]) -> Message:
@@ -37,3 +34,12 @@ class Message:
             body=d["body"],
             auth=d["auth"],
         )
+
+
+@attrs.define
+class MessageInput:
+    id: UUID = attrs.field(converter=UUID)
+
+    channel: dapp.Hash = attrs.field(converter=lambda s: dapp.Hash.from_str(s))
+
+    content: str = attrs.field(validator=message_content_validator)
